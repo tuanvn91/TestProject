ee3afd541bd4fd87ece2f0cb8dc819db870bffe6
tuanv <tuanvn91>
2021-11-17 11:14:38 +0700

    gchgf

--- app/src/main/java/com/tadfas/testproject/MyApplication.java
+++ app/src/main/java/com/tadfas/testproject/MyApplication.java
@@ -2,20 +2,19 @@ package com.tadfas.testproject;
 
 import android.app.Application;
 
-import com.muicv.mutils.AdsManager;
+//import com.muicv.mutils.AdsManager;
 
 
 public class MyApplication extends Application {
-    private AdsManager adsManager;
+//    private AdsManager adsManager;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        AdsManager.DEBUG = BuildConfig.DEBUG;
-        AdsManager.PREFERENCE_NAME = "topvidieodownloader";
-        AdsManager.APPLICATION_ID = getPackageName();
-        adsManager = new AdsManager(this);
-
+//        AdsManager.DEBUG = BuildConfig.DEBUG;
+//        AdsManager.PREFERENCE_NAME = "topvidieodownloader";
+//        AdsManager.APPLICATION_ID = getPackageName();
+//        adsManager = new AdsManager(this);
 
     }
 }
---
+++ app/src/main/java/com/tadfas/testproject/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,52 @@
+package com.tadfas.testproject.ui.dashboard
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.tadfas.testproject.R
+import com.tadfas.testproject.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.d("adstest", "create new instance")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
